e381ffe14f386d10ebbae2b57b85f9fd62103e0d
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:24:07 +0800

    Update L4Q5.java

--- L4/L4Q5.java
+++ L4/L4Q5.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l4q5;
-
 import java.util.Scanner;
 import java.util.Random;
-/**
- *
- * @author Yee Jet Tan
- */
+
 public class L4Q5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int p1_total = 0, p1_score, p2_total = 0, p2_score;
         Scanner in = new Scanner(System.in);
@@ -49,5 +36,4 @@ public class L4Q5 {
             System.out.println("Player 2 wins!");
         }
     }
-    
 }
