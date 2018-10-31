178a1dfa77fd6692b134aaa2e2368e6ffcf8362f
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:25:02 +0800

    Update L4Q6.java

--- L4/L4Q6.java
+++ L4/L4Q6.java
@@ -1,20 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l4q6;
-
 import java.util.Random;
-/**
- *
- * @author Yee Jet Tan
- */
+
 public class L4Q6 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Random r = new Random();
         int num, num_digit = 1;
@@ -31,9 +18,8 @@ public class L4Q6 {
         } else {
             num_digit = 1;
         }
+        
         System.out.println("The integer is " + num);
-        System.out.println("The number of digits in the integer is " + num_digit);
-                
+        System.out.println("The number of digits in the integer is " + num_digit);        
     }
-    
 }
