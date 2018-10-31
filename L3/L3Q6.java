928f177d628694ede592754fb08e2253ae52aed5
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 12:37:50 +0800

    Update L3Q6.java

--- L3/L3Q6.java
+++ L3/L3Q6.java
@@ -1,22 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l3q6;
-
 import static java.lang.Math.sqrt;
 import java.util.Scanner;
 
-/**
- *
- * @author wid180503
- */
 public class L3Q6 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter radius of circle: ");
@@ -33,5 +19,8 @@ public class L3Q6 {
         else {
             System.out.println("Outside of circle");
         }
+        
     }
+    
 }
+
