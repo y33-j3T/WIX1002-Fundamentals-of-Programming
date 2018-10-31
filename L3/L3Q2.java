3b9c99feff55cadc79b8874b470f777487aa7dab
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 12:34:04 +0800

    Update L3Q2.java

--- L3/L3Q2.java
+++ L3/L3Q2.java
@@ -1,24 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l3q2;
-
 import java.util.Random;
 
-/**
- *
- * @author wid180503
- */
 public class L3Q2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Random r = new Random();
         int randInt = r.nextInt(6);
+        
         switch (randInt){
             case 0:
                 System.out.println(randInt + " is" + " zero"  );
@@ -44,7 +31,6 @@ public class L3Q2 {
                 System.out.println(randInt + " is " + " five" );
                 break;
         }
-        
         
     }
     
