af25fcefe1e2f75b3a54c8dd33f44160c422e144
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:30:00 +0800

    Update L5Q3.java

--- L5/L5Q3.java
+++ L5/L5Q3.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l5q3;
-
 import java.util.Random;
 import java.util.Scanner;
-/**
- *
- * @author Yee Jet Tan
- */
+
 public class L5Q3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Random r = new Random();
         Scanner s = new Scanner(System.in);
@@ -40,5 +27,4 @@ public class L5Q3 {
             System.out.println();
         }
     }
-    
 }
