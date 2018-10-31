9f27dc5749fc457886ace148537e9c26a5e4040b
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:29:17 +0800

    Update L5Q2.java

--- L5/L5Q2.java
+++ L5/L5Q2.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l5q2;
-
 import java.util.Random;
 
-/**
- *
- * @author Yee Jet Tan
- */
 public class L5Q2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Random r = new Random();
         boolean duplicate = false;
