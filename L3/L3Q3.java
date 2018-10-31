6c53ed7e98f678003ad667611085dc8a7f4d4e8c
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 12:34:47 +0800

    Update L3Q3.java

--- L3/L3Q3.java
+++ L3/L3Q3.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l3q3;
-
 import java.util.Scanner;
 
-/**
- *
- * @author wid180503
- */
 public class L3Q3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Input sales volume: ");
