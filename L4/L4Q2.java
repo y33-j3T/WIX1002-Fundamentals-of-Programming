e3e02b4d341742b2b1c3df52b3811666b26dd3ab
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:21:25 +0800

    Update L4Q2.java

--- L4/L4Q2.java
+++ L4/L4Q2.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l4q2;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Yee Jet Tan
- */
 public class L4Q2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.print("Enter an integer: ");
         Scanner s = new Scanner(System.in);
