579ca432a6c9542496223aa07b7139c67839c69b
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:30:48 +0800

    Update L5Q4.java

--- L5/L5Q4.java
+++ L5/L5Q4.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l5q4;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Yee Jet Tan
- */
 public class L5Q4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[][] mat = new int[3][3];
