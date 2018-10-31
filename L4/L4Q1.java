8ab1a5b553c91ace330e407b704bd67ad38f5f39
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:20:17 +0800

    Update L4Q1.java

--- L4/L4Q1.java
+++ L4/L4Q1.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package l4q1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Yee Jet Tan
- */
 public class L4Q1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.print("Enter an integer: ");
         Scanner s = new Scanner(System.in);
@@ -27,14 +15,10 @@ public class L4Q1 {
         for (int i = 2; i <= input; i++){
             if (input % i == 0){
                 System.out.print(", " + i);
-            }
-            
-            else continue;
+            } else continue;
         }
         
         System.out.println();
-        
-        
     }
     
 }
