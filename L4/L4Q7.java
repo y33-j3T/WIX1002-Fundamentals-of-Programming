a3bfba1ebd8b493294812f2d733a54a4bf0c6c70
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:26:28 +0800

    Update L4Q7.java

--- L4/L4Q7.java
+++ L4/L4Q7.java
@@ -1,20 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l4q7;
-
 import java.util.Scanner;
-/**
- *
- * @author Yee Jet Tan
- */
+
 public class L4Q7 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         //M = Monthly payment
         //P = Principal amount
