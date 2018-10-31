8e7206d654d9f770feef523ba885d4a7242b7e25
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 11:46:42 +0800

    Update and rename OneRandNum.java to L2Q5.java

--- L2/L2Q5.java
+++ L2/L2Q5.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package onerandnum;
-
 import java.util.Random;
 
-/**
- *
- * @author wid180503
- */
 public class OneRandNum {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int MAX = 10000;
         int total = 0;
@@ -28,15 +14,10 @@ public class OneRandNum {
         do{
             total += num%10;
             num /= 10;
-        }
-        
-        while (num >0);
+        } while (num >0);
         
         System.out.println("The sum of all the digits in the number is " + total);
-        
-        
-        
-        
+
     }
     
 }
