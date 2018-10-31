82325d1b476ac3f075339a02a8fa38309277d479
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 15:27:12 +0800

    Update L4Q8.java

--- L4/L4Q8.java
+++ L4/L4Q8.java
@@ -1,20 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package l4q8;
-
 import java.util.Random;
-/**
- *
- * @author Yee Jet Tan
- */
+
 public class L4Q8 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Random r = new Random();
         int n = r.nextInt(101);
