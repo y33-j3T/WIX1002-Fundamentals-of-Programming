87e76b481993eb8bfd2a71bec58446a4a279e0c8
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 11:47:24 +0800

    Update and rename TimeConvert.java to L2Q4.java

--- L2/L2Q4.java
+++ L2/L2Q4.java
@@ -1,23 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package timeconvert;
-
 import java.util.Scanner;
 
-/**
- *
- * @author wid180503
- */
 public class TimeConvert {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int num, hr, min, sec;
+        
         System.out.print("Enter the number of seconds: ");
         Scanner s = new Scanner(System.in);
         num = s.nextInt();
