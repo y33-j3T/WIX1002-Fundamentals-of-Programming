f1fbb007af9a86ec0f7c964adda24a2e3b57cf53
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 11:18:24 +0800

    Update and rename MyProfile.java to L1Q2.java

---
+++ L1/L1Q2.java
@@ -0,0 +1,19 @@
+
+package l1q2;
+
+
+public class MyProfile{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Full Name: Eggsy");
+        System.out.println("Matric Number: XXX123");
+        System.out.println("Address: 28, Eggsy Road, 45000 Kuala Lumpur, Malaysia" );
+        System.out.println("Email: eggsy@gmail.com");
+        System.out.println("Contact: +6012 222 9999");
+    }
+    
+}
