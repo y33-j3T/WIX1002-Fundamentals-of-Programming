c90a118360b3602f9986f27a00fafd2d7b0e5946
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 11:48:10 +0800

    Update and rename EnergyHeatWater.java to L2Q6.java

--- L2/L2Q6.java
+++ L2/L2Q6.java
@@ -1,29 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package energyheatwater;
-
 import java.util.Scanner;
 
-/**
- *
- * @author wid180503
- */
 public class EnergyHeatWater {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         double Q, tempI, tempF, M;
         Scanner s = new Scanner(System.in);
         
         System.out.print("Weight of water in kg: ");
         M = s.nextDouble();
+        
         System.out.print("Initial temperature in celsius: ");
         tempI = s.nextDouble();
+        
         System.out.print("Final temperature in celsius: ");
         tempF = s.nextDouble();
         
