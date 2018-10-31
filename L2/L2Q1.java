b7da807fc0644b08bd9991eb14507caf589b696d
y33_j3T <41362792+y33-j3T@users.noreply.github.com>
2018-10-31 11:44:37 +0800

    Update and rename DegreeToCelsius.java to L2Q1.java

--- L2/L2Q1.java
+++ L2/L2Q1.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package degreetocelsius;
-
 import java.util.Scanner;
 
-/**
- *
- * @author wid180503
- */
 public class DegreeToCelsius {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         double fah, cel;
         Scanner s = new Scanner(System.in);
@@ -24,6 +10,7 @@ public class DegreeToCelsius {
         fah = s.nextDouble();
         cel = (fah - 32)/1.8;
         System.out.printf("The temperature in degree Celsius is %-5.2f", cel );
+        
     }
     
 }
